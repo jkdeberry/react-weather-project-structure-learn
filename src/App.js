492f91d9441c5bc0478e8a56a423c88488133179
jkdeberry <debejmac@gmail.com>
2025-07-06 16:29:51 -0400

    Added gitbhub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>This project was coded by {" "}Jeanine DeBerry and is {" "} 
+      <a href="https://github.com/jkdeberry/react-weather-project-structure-learn" target="_blank">open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
