90ff1e0a8183b66cbb4430ed077d6f5619b17ef9
jkdeberry <debejmac@gmail.com>
2025-07-07 18:05:43 -0400

    Added heading in h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container"> 
-      <h1></h1>
+      <h1>Weather App</h1>
       <Weather />
       <footer>
         This project was coded by{" "}
