cd2b98c377263d229ddcf9f011722240b2998d0b
jkdeberry <debejmac@gmail.com>
2025-07-07 10:57:06 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container"> 
       <h1>Weather App</h1>
-      <footer>This project was coded by {" "}Jeanine DeBerry and is {" "} 
-      <a href="https://github.com/jkdeberry/react-weather-project-structure-learn" target="_blank">open-sourced on Github
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/jkdeberry" target="_blank"  rel="noopener noreferrer">Jeanine DeBerry</a>{" "}
+        and is {" "} 
+        <a href="https://github.com/jkdeberry/react-weather-project-structure-learn" target="_blank" rel="noopener noreferrer">open-sourced on Github
       </a>
       </footer>
+      </div>
     </div>
   );
 }
-
-
