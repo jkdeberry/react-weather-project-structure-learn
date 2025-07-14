1223db74f38f7a534c21522f7e10633803ef74ae
jkdeberry <debejmac@gmail.com>
2025-07-14 18:32:11 -0400

    Updated FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -3,19 +3,22 @@ import React from "react";
 export default function FormattedDate(props) {
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   let day = days[props.date.getDay()];
+  
   let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
   let minutes = props.date.getMinutes();
+
+
+  let ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  
+
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {hours}:{minutes} {ampm}
     </div>
   );
 }
