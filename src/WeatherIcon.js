5b010f53fde5a1f2591606d28379ca3f4e459964
jkdeberry <debejmac@gmail.com>
2025-07-15 19:26:07 -0400

    Created new component and set temp conversion

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={icon}
-      color="grey"
+      color="black"
       size={45}
       animate={true}
     />
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherInfo(props) {
@@ -19,11 +20,10 @@ export default function WeatherInfo(props) {
             <WeatherIcon code={props.data.icon} 
             /> 
           <div>
-            <span className="temperature">{Math.round(props.data.temperature)}</span>
-            <span className="unit">°F</span>
-          </div>
+            <WeatherTemperature fahrenheit={props.data.temperature} /> 
           </div>
         </div>
+      </div>
       <div className="col-6">
       <ul>
         <li>Humidity: {props.data.humidity}%</li>
