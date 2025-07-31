ef22c31a45d5a020d04580409ba10e57908cd433
jkdeberry <debejmac@gmail.com>
2025-07-31 14:33:20 -0400

    Updated responsive.css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,7 +3,6 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
@@ -16,21 +15,18 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <WeatherIcon code={props.data.icon} size={45} 
-            /> 
-          <div>
-            <WeatherTemperature fahrenheit={props.data.temperature} /> 
+          <div className="weather-main">
+            <WeatherIcon code={props.data.icon} size={64} />
+            <WeatherTemperature fahrenheit={props.data.temperature} />
           </div>
         </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
       </div>
-      <div className="col-6">
-      <ul>
-        <li>Humidity: {props.data.humidity}%</li>
-        <li>Wind: {props.data.wind} km/h</li>
-      </ul>
-      </div>
-    </div>
     </div>
   );
-} 
+}
